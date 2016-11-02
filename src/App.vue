b12050d72f4ff588aef36c89ebe2b83d3e8d9d82
Ras <contact@rasputin.io>
2016-11-02 14:55:12 -0700

    Added main Vue component

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <p>
+    {{message}}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: 'Hello from App.vue!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
