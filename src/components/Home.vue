bdb054ef9aafe27bd255e22a4b66d35819a2472a
Ras <contact@rasputin.io>
2016-11-02 16:10:40 -0700

    Added main pages

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <p>
+    {{message}}
+  </p>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'This is the home page'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <p>
+    {{message}}
+  </p>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'This is the post page'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
