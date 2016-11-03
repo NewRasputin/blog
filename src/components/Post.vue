18275333e25562b3db72703755ad5d841f346fe9
Ras <contact@rasputin.io>
2016-11-02 18:53:47 -0700

    Added post form

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,18 +1,29 @@
 <template lang="html">
-  <p>
-    {{message}}
-  </p>
+  <div >
+    <label for="title">Title:</label>
+    <input v-model="title" name="title" type="text">
+    <label for="body">Body:</label>
+    <textarea v-model="body" name="body"></textarea>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'Post',
   data () {
     return {
-      message: 'This is the post page'
+      title: '',
+      body: ''
     }
   }
 }
 </script>
 
 <style lang="css">
+  textarea {
+    resize: none;
+  }
+  input, label {
+    display: block;
+  }
 </style>
