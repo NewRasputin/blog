d9c3607d4427087b93e15fdecf0fb0c2836b0479
Ras <contact@rasputin.io>
2016-11-05 19:48:46 -0700

    Added SignUp component

---
+++ src/components/SignUp.vue
@@ -0,0 +1,59 @@
+<template lang="html">
+  <div>
+    <div v-if="errmsg" class="right error">
+      <h3>{{errmsg}}</h3>
+    </div>
+    <label for="username">Username:</label>
+    <input type="text" name="username" v-model="username">
+    <label for="password">Password:</label>
+    <input type="text" name="password" v-model="password">
+    <label for="retypepassword">Retype Password:</label>
+    <input type="text" name="retypepassword" v-model="retypepassword">
+    <input type="button" v-on:click="submit" value="Submit">
+    <p v-if="errmsg">{{errmsg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  data () {
+    return {
+      username: '',
+      password: '',
+      retypepassword: '',
+      errmsg: ''
+    }
+  },
+  computed: {
+    passwordsMatch () {
+      return this.password === this.retypepassword
+    }
+  },
+  methods: {
+    submit () {
+      if (this.username) {
+        if (this.passwordsMatch) {
+          this.$http.post('/auth/signup', {
+            username: this.username,
+            password: this.password
+          }).then((res) => {
+            if (res.body === 'Success') {
+              this.$router.push('/')
+            }
+          }, (res) => {
+            this.$set(this.$data, 'errmsg', res.body)
+          })
+        } else {
+          console.error('Passwords don\'t match');
+        }
+      } else {
+        console.error("Username required")
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
