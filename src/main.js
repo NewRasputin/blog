363b012d698b726d08dcbe44349a0b9425a5c7aa
Ras <contact@rasputin.io>
2016-11-05 17:51:48 -0700

    Added signup route

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import Home from './components/Home.vue'
 import Post from './components/Post.vue'
 import Login from './components/Login.vue'
+import SignUp from './components/SignUp.vue'
 import NotFound from './components/NotFound.vue'
 
 
@@ -18,6 +19,7 @@ const router = new VueRouter({
     {path: '/', component: Home},
     {path: '/post', component: Post},
     {path: '/login', component: Login},
+    {path: '/signup', component: SignUp},
     {path: '*', component: NotFound}
   ]
 })
