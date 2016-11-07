8603a5f8a6363bfb54de7de29a5acef410995400
Ras <contact@rasputin.io>
2016-11-07 15:05:30 -0800

    Refactored for sessions

--- routes/api.js
+++ routes/api.js
@@ -7,30 +7,40 @@ var util = require('util')
 // require Post model
 var Post = require('../models/post.js')
 
-api.route('/post')
-  .get(function (req, res) {
-    Post.find({}, function (err, posts) {
-      if (err) {
-        util.log(err)
-      } else {
-        res.send(posts)
-      }
-    })
+var authenticate = require('./middleware.js')
+
+// called on landing on home page, fetches posts
+// should be available to all
+api.get('/post', function (req, res) {
+  util.log("Fetching posts...")
+  Post.find({}, function (err, posts) {
+    if (err) {
+      util.log(err)
+    } else if (posts) {
+      util.log('Posts found!')
+      res.send(posts)
+    } else {
+      util.log('No posts found.')
+    }
   })
-  .post(function (req, res) {
-    util.log('Saving post...')
-    var newPost = new Post({
-      title: req.body.title,
-      body: req.body.body
-    })
-    newPost.save(function (err) {
-      if (err) {
-        util.log(err)
-      } else {
-        util.log('Post saved!')
-        res.send('Success')
-      }
-    })
+})
+
+// called on submitting post form
+// should be protected
+api.post('/post', authenticate, function (req, res) {
+  util.log('Saving post...')
+  var newPost = new Post({
+    title: req.body.title,
+    body: req.body.body
   })
+  newPost.save(function (err) {
+    if (err) {
+      util.log(err)
+    } else {
+      util.log('Post saved!')
+      res.send('Success')
+    }
+  })
+})
 
 module.exports = api
